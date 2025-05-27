d6c83894640726e2135a5131d0fcd3ec8a3aa3e2
lhakiim <luqmanulhakim2021@gmail.com>
2025-05-27 10:40:38 +0700

    updeta dashboard

--- dashboard_mahasiswa/urls.py
+++ dashboard_mahasiswa/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
 
     path('dashboard/mahasiswa/', views.dashboard_mahasiswa, name='dashboard_mahasiswa'),
 
-    path('lowongan/list/', views.list_lowongan_tersedia, name='list_lowongan_tersedia'),
+    path('lowongan/diterima/', views.list_lowongan_diterima, name='list_lowongan_diterima'),
     path('lowongan/daftar/<int:lowongan_id>/', views.daftar_lowongan, name='daftar_lowongan'),
   
     path('logs/my/', views.my_logs, name='my_logs'),
+    
 
 ]
